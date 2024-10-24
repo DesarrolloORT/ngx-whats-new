1edb7b99e0a3539358bdea7a3012a470ca701475
Gastón Carriquiry <carriquiry@ort.edu.uy>
2024-10-24 15:26:08 -0300

    feat(lib): improve open/close functionality and docs

--- projects/ngx-whats-new/src/lib/ngx-whats-new.component.ts
+++ projects/ngx-whats-new/src/lib/ngx-whats-new.component.ts
@@ -25,13 +25,16 @@ const DEFAULT_OPTIONS: Options = {
   styleUrls: ['./ngx-whats-new.component.scss'],
 })
 export class NgxWhatsNewComponent implements AfterViewInit {
-  /** Global options */
+  /** Options item initialized with default values. Accepts custom values. */
   private _options: Options = DEFAULT_OPTIONS;
 
-  /** Modals to show */
+  /** Items to show in the dialog */
   @Input() items: ModalWindow[] = [];
 
-  /** Set some default options */
+  /** Control whether the dialog is visible */
+  protected isVisible = false;
+
+  /** Options for What's New dialog */
   @Input() set options(options: Options) {
     this._options = { ...options };
   }
@@ -39,8 +42,8 @@ export class NgxWhatsNewComponent implements AfterViewInit {
     return this._options;
   }
 
-  /** Emits on close */
-  @Output() closeModal = new EventEmitter<void>();
+  /** Event emitted on dialog close */
+  @Output() private readonly closeModal = new EventEmitter<void>();
 
   /** Reference to the close button */
   @ViewChild('wnCloseButton') private readonly _closeButton?: ElementRef;
@@ -48,6 +51,11 @@ export class NgxWhatsNewComponent implements AfterViewInit {
   /** Index of the selected modal item */
   protected selectedIndex = 0;
 
+  /**
+   * Sets focus to the close button when the component is initialized.
+   *
+   * **USE NOT RECOMMENDED**
+   */
   ngAfterViewInit(): void {
     if (this._closeButton) {
       this._closeButton.nativeElement.focus(); // Set focus to the close button
@@ -55,11 +63,16 @@ export class NgxWhatsNewComponent implements AfterViewInit {
   }
 
   /**
-   * Registers a listener for keyboard-based navigation between modals
+   * Registers a listener for keyboard-based navigation between items.
+   *
+   * Handled events:
+   *  - ArrowRight: next item
+   *  - ArrowLeft: previous item
+   *  - Escape: close dialog
    * @param $event keyboard event
    */
   @HostListener('window:keydown', ['$event'])
-  navigateByKeyboard($event: KeyboardEvent): void {
+  protected handleKeyboardNavigation($event: KeyboardEvent): void {
     if (this._options.enableKeyboardNavigation) {
       switch ($event.key) {
         case 'ArrowRight':
@@ -85,14 +98,12 @@ export class NgxWhatsNewComponent implements AfterViewInit {
    * Registers a listener for Escape key to close the modal
    */
   @HostListener('document:keydown.escape', ['$event'])
-  handleEscape() {
+  protected handleEscape() {
     this.close();
   }
 
-  /**
-   * Navigates to the next modal
-   */
-  goToNext(): void {
+  /** Navigates to the next item. Closes What's New dialog if it is the last one. */
+  public goToNext(): void {
     if (this.selectedIndex < this.items.length - 1) {
       this.selectedIndex = this.selectedIndex + 1;
     } else {
@@ -101,20 +112,24 @@ export class NgxWhatsNewComponent implements AfterViewInit {
   }
 
   /**
-   * Navigates to the modal with the provided index
-   * @param index number of the modal window
+   * Navigates to the item corresponding to the provided number.
+   * @param index number of the item
    */
-  navigateTo(index: number): void {
+  public navigateTo(index: number): void {
     if (this._options.clickableNavigationDots) {
       this.selectedIndex = index;
     }
   }
 
-  /**
-   * Closes the modal dialog
-   */
-  close(): void {
+  /** Opens What's New dialog. */
+  public open(): void {
+    this.isVisible = true;
+  }
+
+  /** Closes What's New dialog. */
+  public close(): void {
     if (!this._options.disableClose) {
+      this.isVisible = false;
       this.closeModal.emit();
     }
   }
