6c61d7b65bfdf1eafb57e2f9add9b0c7b7e73256
Gastón Carriquiry <carriquiry@ort.edu.uy>
2024-10-24 10:08:52 -0300

    chore(lint): run linter autofix

--- projects/test-app/src/app/app.module.ts
+++ projects/test-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+
 import { NgxWhatsNewModule } from '../../../ngx-whats-new/src/lib/ngx-whats-new.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
