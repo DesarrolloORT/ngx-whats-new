9ea4fac1ca63d5ea26e162fcb6a25f0e8d59aad8
Gastón Carriquiry <carriquiry@ort.edu.uy>
2024-10-25 16:43:59 -0300

    test(app): import ngx whats new component to remove warning

--- projects/test-app/src/app/app.component.spec.ts
+++ projects/test-app/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { NgxWhatsNewComponent } from 'projects/ngx-whats-new/src/public-api';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [NgxWhatsNewComponent]
     }).compileComponents();
   });
 
