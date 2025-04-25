7411968c42071c8873ee4ca4517fc309034cd59d
Gastón Carriquiry <carriquiry@ort.edu.uy>
2025-04-25 16:55:08 -0300

    build: transform project to prefer ESM and update Jest to use this syntax

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,5 @@
-import 'jest-preset-angular/setup-jest';
+import { jest as jestGlobal } from '@jest/globals';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone/index.mjs';
+
+globalThis.jest = jestGlobal; // add jest to global scope
+setupZoneTestEnv();
