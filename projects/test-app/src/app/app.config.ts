6380fd105e80289990b00246df42c14e2792d54f
Gastón Carriquiry <carriquiry@ort.edu.uy>
2025-04-28 11:52:00 -0300

    chore: remove utility imported from deleted import

--- projects/test-app/src/app/app.config.ts
+++ projects/test-app/src/app/app.config.ts
@@ -1,15 +1,6 @@
-import {
-  ApplicationConfig,
-  provideAppInitializer,
-  provideExperimentalZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { UiUtils } from '@desarrolloort/ngx-utils';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideAppInitializer(() => UiUtils.initializeMaterialSymbols()),
-    provideAnimations(),
-  ],
+  providers: [provideExperimentalZonelessChangeDetection(), provideAnimations()],
 };
